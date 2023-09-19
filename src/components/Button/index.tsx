6214ca3e935b3dddeb538b1172c1917fa973d7d3
JavierBalonga <javierbalonga@gmail.com>
2023-09-18 21:06:36 -0300

    fix(Button) add border to filled variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -30,29 +30,29 @@ export default function Button({
   return (
     <button
       className={twMerge(
-        "px-6 py-3 font-bold text-sm rounded-lg transition-colors",
+        "px-6 py-3 font-bold text-sm rounded-lg transition-colors border",
         variant === "filled"
           ? color === "primary"
-            ? "bg-primary hover:bg-primary-light active:bg-primary-dark"
+            ? "bg-primary border-primary hover:bg-primary-light active:bg-primary-dark"
             : color === "white"
-            ? "bg-neutral-200 text-neutral-900 hover:bg-neutral-50 active:bg-neutral-400"
+            ? "bg-neutral-200 border-neutral-200 text-neutral-900 hover:bg-neutral-50 active:bg-neutral-400"
             : color === "error"
-            ? "bg-error hover:bg-error-light active:bg-error-dark"
+            ? "bg-error border-error hover:bg-error-light active:bg-error-dark"
             : color === "success"
-            ? "bg-success hover:bg-success-light active:bg-success-dark"
+            ? "bg-success border-success hover:bg-success-light active:bg-success-dark"
             : color === "warning"
-            ? "bg-warning hover:bg-warning-light active:bg-warning-dark"
+            ? "bg-warning border-warning hover:bg-warning-light active:bg-warning-dark"
             : ""
           : color === "primary"
-          ? "border border-primary text-primary hover:bg-primary-light active:bg-primary-dark"
+          ? "border-primary text-primary hover:bg-primary-light active:bg-primary-dark"
           : color === "white"
-          ? "border border-neutral-200 text-neutral-200 hover:bg-neutral-50 active:bg-neutral-400"
+          ? "border-neutral-200 text-neutral-200 hover:bg-neutral-50 active:bg-neutral-400"
           : color === "error"
-          ? "border border-error text-error hover:bg-error-light active:bg-error-dark"
+          ? "border-error text-error hover:bg-error-light active:bg-error-dark"
           : color === "success"
-          ? "border border-success text-success hover:bg-success-light active:bg-success-dark"
+          ? "border-success text-success hover:bg-success-light active:bg-success-dark"
           : color === "warning"
-          ? "border border-warning text-warning hover:bg-warning-light active:bg-warning-dark"
+          ? "border-warning text-warning hover:bg-warning-light active:bg-warning-dark"
           : "",
         className
       )}

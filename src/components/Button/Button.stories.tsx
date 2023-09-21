20faf0f53c553336678661f697b3cb75f6d21560
JavierBalonga <javierbalonga@gmail.com>
2023-09-21 19:56:51 -0300

    feat(*) add Dark Mode to docs

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,13 +1,13 @@
-import "../../index.css";
-
 import type { Meta, StoryObj } from "@storybook/react";
-
+import { themes } from "@storybook/theming";
+import "../../index.css";
 import Button from "./index";
 
 const meta = {
   title: "Button",
   component: Button,
   tags: ["autodocs"],
+  parameters: { docs: { theme: themes.dark } },
   args: {
     children: "Button",
   },
--- src/components/TextInput/TextInput.stories.tsx
+++ src/components/TextInput/TextInput.stories.tsx
@@ -1,14 +1,14 @@
-import "../../index.css";
-import EyeIcon from "../icons/EyeIcon";
-
 import type { Meta, StoryObj } from "@storybook/react";
-
+import { themes } from "@storybook/theming";
+import "../../index.css";
 import TextInput from "./index";
+import EyeIcon from "../icons/EyeIcon";
 
 const meta = {
   title: "TextInput",
   component: TextInput,
   tags: ["autodocs"],
+  parameters: { docs: { theme: themes.dark } },
   args: {
     label: "Label",
     placeholder: "Placeholder",

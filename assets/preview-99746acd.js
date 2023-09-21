bd5c202e82ab7d70bdf2f3c58b3d891123940426
JavierBalonga <JavierBalonga@users.noreply.github.com>
2023-09-21 23:24:52 +0000

    deploy: b4e69e26239f7e02646ce98242d27667cd7975c7

---
+++ assets/preview-99746acd.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-200463d0.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-cdd19688.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-cdd19688.js","./iframe-200463d0.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./_getPrototype-ddf2eac9.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-99746acd.js.map
